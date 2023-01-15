bdbd1132f2f53afde236fa41de744490e5dd9b57
pavel <pavel@scalecrm.com>
2023-01-15 17:18:36 +0200

    added helper functions for import and destroy data in DB

---
+++ backend/seeder.js
@@ -0,0 +1,57 @@
+import User from "./models/userSchema.js";
+import Product from "./models/productSchema.js";
+import Order from "./models/orderSchema.js";
+import dotenv from 'dotenv'
+import users from '../backend/data/users.js'
+import products from '../backend/data/products.js'
+import connectDB from './config/db.js'
+import colors from 'colors'
+
+dotenv.config()
+
+connectDB()
+
+const importData = async() => {
+    try {
+        await Product.deleteMany()
+        await Order.deleteMany()
+        await User.deleteMany()
+
+        const createdUsers = await User.insertMany(users)
+        console.log('createdUsers', createdUsers)
+
+        const adminUser = createdUsers[0]._id
+
+        const sampleProducts = await products.map(item => {
+            return {...item, user: adminUser}
+        })
+        console.log('sampleProducts', sampleProducts)
+        await Product.insertMany(sampleProducts)
+
+        console.log('Imported data'.green.bold)
+        process.exit()
+    } catch(e) {
+        console.error(`${e}`.red.bold)
+        process.exit(1)
+    }
+}
+
+const destroyData = async() => {
+    try {
+        await Product.deleteMany()
+        await Order.deleteMany()
+        await User.deleteMany()
+
+        console.log('Destroyed data')
+        process.exit()
+    } catch(e) {
+        console.error(`${e}`.red.bold)
+        process.exit(1)
+    }
+}
+
+if(process.argv[2] === '-d') {
+    destroyData()
+} else {
+    importData()
+}
